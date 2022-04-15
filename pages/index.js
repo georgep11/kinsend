c32644991e5f9073e966cd48ef7ced1b68a50f9d
georgep11 <georgepdev20@outlook.com>
2022-04-15 21:18:25 +0300

    Fix error datetime picker

--- pages/index.js
+++ pages/index.js
@@ -77,6 +77,7 @@ export default function Home({isConnected, listingsAndReviews}) {
       <KeyboardDateTimePicker
         variant="inline"
         margin="normal"
+        onChange={() => {}}
       />
     </div>
 
