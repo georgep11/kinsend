caba1e562ed2cf059153f434f1d9b24a883f4d70
georgep11 <georgepdev20@outlook.com>
2022-04-18 02:01:46 +0300

    hover styles for buttons and inputs

--- infrastructure/theme/AppTheme.js
+++ infrastructure/theme/AppTheme.js
@@ -8,9 +8,22 @@ export const AppTheme = createTheme({
       root: {
         backgroundColor: '#F1F2F7',
         borderColor: '#DCDCDC',
-        color: '#707070'
+        color: '#707070',
+        '&:hover': {
+          color: '#000000',
+          fontWeight: 700
+        }
       }
     },
+    MuiButton: {
+      outlinedPrimary: {
+        '&:hover': {
+          backgroundColor: '#D15D36',
+          color: 'white',
+          fontWeight: 700
+        }
+      }
+    }
   },
   palette: {
     primary: {
