141dc9a4af7c6d7fcdcee11d24295f7974094100
georgep11 <georgepdev20@outlook.com>
2022-06-02 19:49:17 +0300

    feat: form submission

--- src/pages/Public/ThankYouSubmission/index.js
+++ src/pages/Public/ThankYouSubmission/index.js
@@ -1,21 +1,42 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+// import { CheckCircleOutlined } from "@ant-design/icons";
+import { LogoIcon } from "../../../assets/svg";
 
-import {
-  selectPublic,
-} from "../../../redux/publicReducer";
+import { selectPublic } from "../../../redux/publicReducer";
+import "./styles.less";
 
 const ThankYouSubmission = () => {
-  const { addedFormSubmission } =
-  useSelector(selectPublic);
+  const { addedFormSubmission } = useSelector(selectPublic);
 
-  return <div className="thank-you-page">
-    <div className="max-w-screen-md	mx-auto mb-5 mt-14">
-      <h1 className="mb-4 text-center">Thank you</h1>
-      <p className="text-center">{addedFormSubmission?.form?.message}</p>
-      <a className="mt-5 block" href={addedFormSubmission?.owner?.vCard?.url} download>VCard.vcf</a>
+  return (
+    <div className="thank-you-page">
+      <div className="thank-you-box text-center">
+        <p className="text-center mb-4 text-2xl font-bold">
+          {addedFormSubmission?.form?.message ||
+            "Thank you for adding yourself to my phone book!"}
+        </p>
+        <LogoIcon />
+        <p className="note inline-flex">
+          Want your own?{" "}
+          <a
+            href={`${window.origin}/ref=${addedFormSubmission?.owner?.id}`}
+            className="block font-bold ml-2 hover:underline"
+          >
+            Click here
+          </a>
+        </p>
+        <a
+          className="mt-3 block font-bold"
+          href={addedFormSubmission?.owner?.vCard?.url}
+          download
+          title={`${addedFormSubmission?.owner?.firstName}-${addedFormSubmission?.owner?.lastName}`}
+        >
+          Download: VCard.vcf
+        </a>
+      </div>
     </div>
-  </div>
-}
+  );
+};
 
 export default ThankYouSubmission;
