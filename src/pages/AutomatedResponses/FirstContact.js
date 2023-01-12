6c3a8a905f64e0d6970b96391460a2bbb6ffcaba
georgep11 <georgepdev20@outlook.com>
2023-01-12 15:28:20 +0200

    feat: first contact - fix layout on mobile

--- src/pages/AutomatedResponses/FirstContact.js
+++ src/pages/AutomatedResponses/FirstContact.js
@@ -11,30 +11,32 @@ const FirstContact = () => {
   const dispatch = useDispatch();
 
   return (
-    <LayoutComponent className="settings-page" title="Setting">
-      <div className="flex justify-between">
+    <LayoutComponent className="settings-page" title="First Contact">
+      <div className="flex flex-col md:flex-row justify-between">
         <div>
-          <div className="flex items-center">
+          <div className="flex items-center justify-between md:justify-start">
             <h1 className="text-3xl md:text-4xl">
               First Contact
             </h1>
             <Switch className="ml-5" defaultChecked onChange={(checked) => dispatch(toggleFirstContactAsync(checked))} />
           </div>
-          <div className="max-w-4xl mt-4">
+          <div className="max-w-4xl mt-2 md:mt-4">
             Customize the automatic response that attempts to send non-subscribers to your subscriber intake form when they text you for the first time. 
             There will be a 2 min human-emulated delay before your automated response is selected. 
             If they don't complete the form after 30 min, you can trigger an additional reminder message. 
             We don’t allow more than 2 messages to be sent to non-subscribers.
           </div>
         </div>
-        <Button
-          type="primary"
-          size="large"
-          className="inline-flex items-center px-10 mt-5 md:mt-0"
-          onClick={console.log}
-        >
-          Save
-        </Button>
+        <div className="flex">
+          <Button
+            type="primary"
+            size="large"
+            className="inline-flex items-center px-10 mt-5 md:mt-0 m-auto"
+            onClick={() => console.log}
+          >
+            Save
+          </Button>
+        </div>
       </div>
     </LayoutComponent>
   );
